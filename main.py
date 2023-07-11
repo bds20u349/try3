70a1423ff553e573acbd2a73f56ec26ca3c65eff
Daniil <kkdaniilb@gmail.com>
2023-07-11 21:58:22 +0300

    the project after the completion of the tributes

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from name_iops_avg import Output
-from name_iops_avg import Address_format
+from name_iops_avg import Checking_the_file_name
 from export_excel import ExportExcel
 import os
 
-#catalog_name = input('введите путь к файлу: ')
-catalog_name = "C:/Users/kkdan/Desktop/git/project1/rw6d4-16kstrip1800slong"
-catalog_name = catalog_name.replace('\\', '/')
-catalog = os.listdir(catalog_name)
+file = open('Config.txt', 'r') #открываем файл, в торый записаны пути к каталогу с тестами и к котологу с excel файлом
+catalog_name = file.readline()[13:-1] #первые 13 символов - это "folder name: ", а последний знак переноса "\n"
+file.close() #закрываем файл
+catalog_name = catalog_name.replace('\\', '/') #заменяем системные знаки \ на /
+folder = input('введите имя папки: ') #rw6d4-16kstrip1800slong
+catalog_name = catalog_name + '/' + folder #Получаем общий путь внутрь каталога
+catalog = os.listdir(catalog_name) #получаем список названий всех файлов внутри каталога
 
 for file_name in catalog:
-    name, iops, avg = Output(catalog_name + '/' + file_name)
-    if name != 'Error':
-        ExportExcel(name, iops, avg)
+    if Checking_the_file_name(file_name): #проверка на соответствие имени файла шалону вида "fio-*-*.log"
+        name, iops, avg = Output(catalog_name + '/' + file_name) #Получение из файла с результатом теста имени тестируемого диска, отправленных iops и результата evg
+        ExportExcel(name, iops, avg) #записываем данные в excel
--- name_iops_avg.py
+++ name_iops_avg.py
@@ -1,7 +1,7 @@
-def Chislo(line, start, typ):
+def Reading_the_necessary_information(line, start, typ): #функция, считывающая из строки line подстроку, начинающююся с индекса start и заканчивающуюся пропуском или запятой
     number = ''
     flag = True
-    while flag:
+    while flag: #пока не " " или "," считываем строку
         simvol = line[start]
         if (simvol == ',') or (simvol == ' '):
             flag = False
@@ -9,11 +9,15 @@ def Chislo(line, start, typ):
             number += simvol
         start += 1
 
-    mnogitel = 1
-    if number[-1] == 'k':
+    mnogitel = 1 #если это число, оканчивающееся на k или m, то домножим его на 1000 и 1000000 соостветственно
+    if (number[-1] == 'k') and (typ != 'str'):
         mnogitel = 1000
+        number = number[:-1] #вырежим последний символ, т.к. он не является числом
+    elif (number[-1] == 'm') and (typ != 'str'):
+        mnogitel = 1000000
         number = number[:-1]
-    
+
+    #перевод результата в нужный формат
     if typ == 'int':
         number = int(number)
     if typ == 'float':
@@ -22,37 +26,37 @@ def Chislo(line, start, typ):
         number = number
     return number
 
+def Checking_the_file_name(file_name): #проверка на соответствие имени файла шалону вида "fio-*-*.log"
+    x = file_name[4:].find('-')+4 #Ищем индекс второго знака "-" между подстроками "fio-" и ".log"
+    if (x != -1) and (file_name[:4] == 'fio-') and (file_name[-4:] == '.log') and (file_name[4:x].isdigit()) and (file_name[x+1:-4].isdigit()): #проверяем, чтобы имя файла начиналось на "fio-", заканчивалось на ".log", присутствовал второй знак "-" и между этими тремя подстроками находились числа
+        return True
+    else:
+        return False
+
 def Output(file_name):
     file = open(file_name, 'r')
+    a = True
     try:
-        a = True
         while a:
             line = file.readline()
     
             x = line.find("filename")
             if x != -1:
-                name = Chislo(line, x+14, 'str')
+                name = Reading_the_necessary_information(line, x+14, 'str')
             
             x = line.find("IOPS")
             if x != -1:
-                iops = Chislo(line, x+5, 'float')
+                iops = Reading_the_necessary_information(line, x+5, 'float')
                 
             x = line.find(" lat")
             if x != -1:
-                avg = Chislo(line, line.find("avg")+4, 'float')
+                if line.find("usec") != -1:
+                    mnogitel = 1000
+                else:
+                    mnogitel = 1
+                avg = Reading_the_necessary_information(line, line.find('avg')+4, 'float')/mnogitel
                 a = False
-            if line == '':
-                a = False
-                name = 'Errore'
-                iops = -1
-                avg = -1
         return name, iops, avg
     finally:
        file.close()
 
-def Address_format(catalog_name):
-    x = catalog_name.find("\\")
-    while x != -1:
-        catalog_name[x] = '1'
-        x = catalog_name.find("\\")
-    return catalog_name
